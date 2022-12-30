78c75d48688552b5250093e340d3062600ef2a8b
Matthew Torres <66329929+Krash-Nebula@users.noreply.github.com>
2022-12-30 10:25:07 -0500

    Perlin noise and seed generation

--- Minecraft Clone Project/Assets/Scripts/Chunk.cs
+++ Minecraft Clone Project/Assets/Scripts/Chunk.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chunk : MonoBehaviour
+public class Chunk
 {
-    public MeshRenderer meshRenderer;
-    public MeshFilter meshFilter;
+    public ChunkCoord coord;
+
+    GameObject chunkObject;
+    MeshRenderer meshRenderer;
+    MeshFilter meshFilter;
 
     int vertexIndex = 0;
     List<Vector3> vertices = new List<Vector3> ();
@@ -16,9 +19,18 @@ public class Chunk : MonoBehaviour
 
     World world;
 
-    void Start()
+    public Chunk (ChunkCoord _coord, World _world)
     {
-        world = GameObject.Find("World").GetComponent<World>();        
+        coord = _coord;
+        world = _world;
+        chunkObject = new GameObject();
+        meshFilter = chunkObject.AddComponent<MeshFilter>();
+        meshRenderer = chunkObject.AddComponent<MeshRenderer>();
+
+        meshRenderer.material = world.material;
+        chunkObject.transform.SetParent(world.transform);
+        chunkObject.transform.position = new Vector3(coord.x*VoxelData.ChunkWidth, 0f, coord.z*VoxelData.ChunkWidth);
+        chunkObject.name = "Chunk " + coord.x + ", " + coord.z;
 
         PopulateVoxelMap();
         CreateMeshData();
@@ -26,7 +38,8 @@ public class Chunk : MonoBehaviour
 
     }
     
-    void CreateMeshData(){
+    void CreateMeshData()
+    {
         for(int y = 0; y< VoxelData.ChunkHeight; y++){
             for(int x=  0; x < VoxelData.ChunkWidth; x++){
                 for(int z = 0; z< VoxelData.ChunkWidth; z++){
@@ -36,30 +49,44 @@ public class Chunk : MonoBehaviour
         }
     }
 
-    bool CheckVoxel (Vector3 pos){
+    public bool isActive
+    {
+        get {return chunkObject.activeSelf; }
+        set {chunkObject.SetActive(value); }
+    }
+
+    public Vector3 position
+    {
+        get {return chunkObject.transform.position; }
+    }
+
+    bool IsVoxelInChunk(int x, int y, int z)
+    {
+        if(x < 0 || x > VoxelData.ChunkWidth - 1 || y < 0 || y > (VoxelData.ChunkHeight - 1) || z < 0 || z > (VoxelData.ChunkWidth - 1))
+            return false;
+        else
+            return true;
+    }
+
+    bool CheckVoxel (Vector3 pos)
+    {
         int x = Mathf.FloorToInt(pos.x);
         int y = Mathf.FloorToInt(pos.y);
         int z = Mathf.FloorToInt(pos.z);
 
-        if(x < 0 || x > VoxelData.ChunkWidth - 1 || y < 0 || y > (VoxelData.ChunkHeight - 1) || z < 0 || z > (VoxelData.ChunkWidth - 1))
-            {
-               return false;
-            }        
+        if(!IsVoxelInChunk(x,y,z))
+            return world.blocktypes[world.GetVoxel(pos + position)].isSolid;
+
         return world.blocktypes[voxelMap[x, y, z]].isSolid;
     }
 
-    void PopulateVoxelMap(){
-
+    void PopulateVoxelMap()
+    {
         for(int y = 0; y< VoxelData.ChunkHeight; y++){
             for(int x=  0; x < VoxelData.ChunkWidth; x++){
                 for(int z = 0; z< VoxelData.ChunkWidth; z++){
                     
-                    if(y < 1)
-                        voxelMap[x, y, z] = 0;
-                    else if (y == VoxelData.ChunkHeight - 1)
-                        voxelMap[x, y, z] = 2;
-                    else
-                        voxelMap[x, y, z] = 1;
+                    voxelMap[x, y, z] = world.GetVoxel(new Vector3(x, y, z) + position);
                     
                     
                 }
@@ -67,7 +94,8 @@ public class Chunk : MonoBehaviour
         }    
     }
 
-    void AddVoxelDataToChunk(Vector3 pos) {
+    void AddVoxelDataToChunk(Vector3 pos) 
+    {
         for(int p = 0; p < 6; p++){
 
             if(!CheckVoxel(pos + VoxelData.faceChecks[p])){
@@ -123,3 +151,24 @@ public class Chunk : MonoBehaviour
     }
 
 }   
+public class ChunkCoord
+{
+     public int x;
+     public int z;
+
+    public ChunkCoord(int _x, int _z)
+    {
+        x = _x;
+        z = _z;
+    }
+
+    public bool Equals(ChunkCoord other)
+    {
+        if(other == null)
+            return false;
+        else if(other.x == x && other.z == z)
+            return true;
+        else
+            return false;
+    }
+}
